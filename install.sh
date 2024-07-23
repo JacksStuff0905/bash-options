b65507e18a4aeef164920ef4ca45b4f37617c889
JacksStuff0905 <114748666+JacksStuff0905@users.noreply.github.com>
2024-07-23 14:03:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 
 script_dir="$(dirname "$(realpath "$0")")"
 
-sudo chmod +x $script_dir/options
-sudo mv $script_dir/options /usr/local/bin/
+sudo chmod +x $script_dir/bash-options
+sudo mv $script_dir/bash-options /usr/local/bin/
 
 sudo chmod +x $script_dir/vcomp
 sudo mv $script_dir/vcomp /usr/local/bin/
